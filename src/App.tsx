d80d5a887b6f3ed58638dfbf77adaf20d5234583
Leonardo <leonardo_barrosocosta@hotmail.com>
2022-06-22 11:18:44 -0300

    finalizando sideBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,7 @@
-import { gql, useQuery } from '@apollo/client';
-import { client } from './lib/apollo';
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id,
-      title
-    }
-  }
-`
-
-interface LessonParams {
-  id: string;
-  title: string;
-}
-
+import { Event } from './pages/Event';
 
 export function App() {
-  const { data } = useQuery<{ lessons: LessonParams[] }>(GET_LESSONS_QUERY)
-
-  
-
   return (
-    <ul>
-      { data?.lessons.map(({ id, title }) => {
-        return <li key={id}>
-          {title}
-        </li>
-      })
-
-      }
-    </ul>
+    <Event />
   )
 }
