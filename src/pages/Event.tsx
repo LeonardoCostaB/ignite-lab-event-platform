5eddb87ac40a97d0bb07e26b407f1a5c30f02c47
Leonardo Costa <leonardo_barrosocosta@hotmail.com>
2022-10-08 16:14:17 -0300

    alterando token

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+
 
 import { Header } from "../components/Header";
 import { SideBar } from "../components/Sidebar";
